38380af451307852414513f49168d7e093bd1c45
kyo <dmsry060209@gmail.com>
2023-04-14 22:10:14 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,11 @@
+n = int(input())
+score = list(map(int, input().split()))
+m = max(score)
+sum = 0
+
+for i in range(n):
+    score[i] = (score[i]/m)*100 
+    sum += score[i]
+
+avg = sum/n
+print(avg)
