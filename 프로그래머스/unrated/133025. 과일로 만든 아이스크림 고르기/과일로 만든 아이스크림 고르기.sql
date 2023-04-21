36aed5da85b82305a9653820b13cd5bf58c9b707
kyo <dmsry060209@gmail.com>
2023-04-21 15:01:59 +0900

    lower the letters

--- 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT fh.flavor 
+select fh.flavor 
 from first_half fh, icecream_info ii
-where fh.flavor = ii.flavor and fh.total_order >= 3000 and ii.INGREDIENT_TYPE = 'fruit_based';
+where fh.flavor = ii.flavor and fh.total_order >= 3000 and ii.ingredient_type = 'fruit_based';
