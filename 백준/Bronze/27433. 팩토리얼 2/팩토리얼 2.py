cb1522806f11babb05c270be2be37351055bf865
kyo <dmsry060209@gmail.com>
2023-04-24 21:29:26 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else :
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+n = int(input())
+print(factorial(n))
