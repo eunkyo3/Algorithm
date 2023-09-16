33c3917767c22b796bc8d95b96b4f2b0bc7d3039
kyo <dmsry060209@gmail.com>
2023-09-16 13:33:12 +0900

    [Silver V] Title: 암호해독기, Time: 104 ms, Memory: 37940 KB -BaekjoonHub

--- 백준/Silver/17176. 암호해독기/암호해독기.py
+++ 백준/Silver/17176. 암호해독기/암호해독기.py
@@ -1,28 +1,28 @@
 n = int(input())
-lis = list(map(int, input().split(" ")))
-s = input()
-number= []
-# A~Z = 1-26 -> ord("A") - 64 / a~z = 27-52 -> ord("a") - 70
 
-if len(s) > len(lis):
-    print("n")
+numlist = list(map(int, input().split()))
+str = input()
+encryption = []
+
+if len(str) != len(numlist):
+    print('n')
 else:
-    for i in s:
-        if i.islower():
-            number.append(ord(i) - 70)
-        elif i.isupper():
-            number.append(ord(i) - 64)
-        else:
-            number.append(0)
+    for i in str:
+        if i.isupper():
+            encryption.append(ord(i) - 64)
+        elif i.islower():
+            encryption.append(ord(i) - 70)
+        elif i == ' ':
+            encryption.append(0)
 
-    lis.sort()
-    number.sort()
+    numlist.sort()
+    encryption.sort()
     flag = 0
 
-    for i in range(len(lis)):
-        if lis[i] != number[i]:
+    for i in range(len(numlist)):
+        if numlist[i] != encryption[i]:
             print("n")
             flag = 1
             break
     if flag == 0:
-        print("y")
+        print("y")    
