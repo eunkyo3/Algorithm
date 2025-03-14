ff0ef8f194187623fb8dbf2a52881c411129abaa
kyo <dmsry060209@gmail.com>
2025-03-14 19:34:33 +0900

    [Silver V] Title: 최소공배수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+a, b = map(int, input().split())
+c = gcd(a, b)
+print((a*b)//c)
